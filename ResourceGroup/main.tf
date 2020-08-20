ebce836e63042c16e4f9d9d4faa797506ad2ae08
Nadeem Ahmed <Nadeemalvi@users.noreply.github.com>
2020-08-20 17:45:42 +0530

    Update main.tf

--- ResourceGroup/main.tf
+++ ResourceGroup/main.tf
@@ -2,11 +2,14 @@ provider "azurerm" {
     version = "2.22.0"
     subscription_id = var.subscriptionID
     tenant_id = var.tenantID
+    client_id = "1d27002f-29c0-4755-b106-c6ef550a3610"
+    client_secret = "otNKzvOfd5Q-QAlmAloWgNJ-7_F33s-~-Z"
     features {}
 }
 
 resource "azurerm_resource_group" "airbus-rsgroup" {
-    name = var.resourceGroupName
+    name = var.resourceGroupName"1d27002f-29c0-4755-b106-c6ef550a3610"
+    client_secret = "otNKzvOfd5Q-QAlmAloWgNJ-7_F33s-~-Z"
     location = var.location
 
     tags = {
